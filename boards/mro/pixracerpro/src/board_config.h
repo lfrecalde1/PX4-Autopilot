1df84545289a74da69a4a0c8c982fa4fc7148ccd
lfrecalde1 <lfrecalde1@espe.edu.ec>
2025-07-23 17:19:13 +0000

    RPM setup

--- boards/mro/pixracerpro/src/board_config.h
+++ boards/mro/pixracerpro/src/board_config.h
@@ -147,6 +147,7 @@
 
 #define BOARD_NUM_IO_TIMERS 3
 #define BOARD_DMA_ALLOC_POOL_SIZE 5120 /* This board provides a DMA pool and APIs */
+#define BOARD_DMA_NUM_DSHOT_CHANNELS 2
 #define BOARD_HAS_ON_RESET 1 /* This board provides the board_on_reset interface */
 #define BOARD_ENABLE_CONSOLE_BUFFER
 
