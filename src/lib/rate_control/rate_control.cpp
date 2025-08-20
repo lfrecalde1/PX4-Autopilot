2e88145ccdcea28c6c7e96b6df14249f80ba4cbf
lfrecalde1 <lfrecalde1@espe.edu.ec>
2025-08-20 14:33:14 +0000

    updates

--- src/lib/rate_control/rate_control.cpp
+++ src/lib/rate_control/rate_control.cpp
@@ -77,8 +77,8 @@ Vector3f RateControl::update(const Vector3f &rate, const Vector3f &rate_sp, cons
 	Vector3f rate_error = rate_sp - rate;
 
 	// PID control with feed forward
-	//const Vector3f torque = _gain_p.emult(rate_error) + _rate_int - _gain_d.emult(angular_accel) + _gain_ff.emult(rate_sp);
-	const Vector3f torque = _gain_p.emult(rate_error) - _gain_d.emult(angular_accel) + _gain_ff.emult(rate_sp);
+	const Vector3f torque = _gain_p.emult(rate_error) + _rate_int - _gain_d.emult(angular_accel) + _gain_ff.emult(rate_sp);
+	//const Vector3f torque = _gain_p.emult(rate_error) - _gain_d.emult(angular_accel) + _gain_ff.emult(rate_sp);
 
 	//PX4_INFO("Gains P: [%.3f, %.3f, %.3f] D: [%.3f, %.3f, %.3f] FF: [%.3f, %.3f, %.3f]",
 	        //(double)_gain_p(0), (double)_gain_p(1), (double)_gain_p(2),
